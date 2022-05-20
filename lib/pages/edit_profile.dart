46b32f4fd6f564893f06844850a7db51a3ecdbc2
UniversoPrematuro <universoprematuro@gmail.com>
2022-05-20 14:16:20 -0300

    upload photo DONE!!!!!!!!!!!!!!!!!!!!

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -1,4 +1,11 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import '../models/perfil_model.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class EditProfile extends StatefulWidget {
   EditProfile({Key? key}) : super(key: key);
@@ -14,9 +21,77 @@ class _EditProfileState extends State<EditProfile> {
   final TextEditingController _controllerBirth = TextEditingController();
   final TextEditingController _controllerGage = TextEditingController();
   final TextEditingController _controllerGender = TextEditingController();
-  
-  
+  FirebaseStorage storage = FirebaseStorage.instance;
+  XFile? _imagem;
+  String? _idUsuarioLogado;
+  bool _subindoImagem = false;
+  String _urlImagemRecuperada = "";
+ 
+  Future _recuperarImagem(String origemImagem) async {
+    final ImagePicker _picker = ImagePicker();
+    XFile? imagemSelecionada;
+    FirebaseStorage storage = FirebaseStorage.instance;
+    if (origemImagem == "camera") {
+      imagemSelecionada = await _picker.pickImage(source: ImageSource.camera);
+      setState(() {
+        _imagem = imagemSelecionada;
+        if (_imagem != null) {
+          _uploadImagem();
+          _subindoImagem = true;
+        }
+      });
+    } else if (origemImagem == "galeria") {
+      imagemSelecionada = await _picker.pickImage(source: ImageSource.gallery);
+    }
+    setState(() {
+      _imagem = imagemSelecionada;
+      if (_imagem != null) {
+        _uploadImagem();
+        _subindoImagem = true;
+      }
+    });
+  }
+ 
+  Future _uploadImagem() async {
+    File file = File(_imagem!.path);
+    Reference pastaRaiz = await storage.ref();
+    Reference arquivo =
+        await pastaRaiz.child("perfil").child("${_idUsuarioLogado!}.jpg");
+ 
+    UploadTask task = arquivo.putFile(file);
+ 
+    task.snapshotEvents.listen((TaskSnapshot storageEvent) {
+      if (storageEvent.state == TaskState.running) {
+        setState(() {
+          _subindoImagem = true;
+        });
+      } else if (storageEvent.state == TaskState.success) {
+        setState(() {
+          _subindoImagem = false;
+        });
+      }
+    });
+ 
+    task.then((TaskSnapshot taskSnapshot) => _recuperarURLimagem(taskSnapshot));
+  }
+ 
+  Future _recuperarURLimagem(TaskSnapshot taskSnapshot) async {
+    String url = await taskSnapshot.ref.getDownloadURL();
+    setState(() {
+      _urlImagemRecuperada = url;
+    });
+  }
+   _recuperarDados() {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User usuarioLogado = auth.currentUser!;
+    _idUsuarioLogado = usuarioLogado.uid;
+  }
 
+  @override
+  void initState() {
+    super.initState();
+    _recuperarDados();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,20 +120,32 @@ class _EditProfileState extends State<EditProfile> {
           child: SingleChildScrollView(
             child: Column(
               children: <Widget>[
-                const CircleAvatar(
-                     radius: 100,
-                     backgroundImage: null,
-                ),
+                CircleAvatar(
+                  radius: 100,
+                  backgroundColor: Colors.grey,
+                  backgroundImage:
+                  _urlImagemRecuperada != null
+                      ? NetworkImage(_urlImagemRecuperada) 
+                      : null),
+                    Padding(
+                      padding: const EdgeInsets.only(),
+                      child: _subindoImagem ? const CircularProgressIndicator(color: Colors.green,) : Container()
+                      )
+                ,
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     TextButton(
-                      child: const Text("Câmera"),
-                      onPressed: () {},
+                      child: const Text("Câmera", style: TextStyle(color: Colors.green)),
+                      onPressed: () {
+                        _recuperarImagem("camera");
+                      },
                     ),
                     TextButton(
-                      child: const Text("Galeria"),
-                      onPressed: () {},
+                      child: const Text("Galeria", style: TextStyle(color: Colors.green)),
+                      onPressed: () {
+                        _recuperarImagem("galeria");
+                      },
                     ),
                   ],
                 ),
